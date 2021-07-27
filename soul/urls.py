6459c79ac4305f7de230b22ba4314bd07d1156dd
ananduv2 <ananduv2.0@gmail.com>
2021-07-27 18:41:58 +0530

    Leads in Pipeline listed for sales account

--- soul/urls.py
+++ soul/urls.py
@@ -60,6 +60,7 @@ urlpatterns = [
     path('sales_dashboard/', SalesDashboard.as_view(),name='sales_dashboard'),
     path('lead/create/', CreateLead.as_view(),name='create_lead'),
     path('lead/new/', MyNewLead.as_view(),name='new_lead'),
+    path('lead/in_pipeline/', MyLeadInPipeline.as_view(),name='lead_in_pipeline'),
 
 
 
